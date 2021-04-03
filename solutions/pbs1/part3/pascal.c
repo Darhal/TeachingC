28bbe597c94d19818687fdb72791962eb06e509c
Omarito <chidaomar46@gmail.com>
2021-04-03 22:22:44 +0200

    Added another solution for pascal

--- solutions/pbs1/part3/pascal.c
+++ solutions/pbs1/part3/pascal.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+////// méthode 1 : solution mathématique
 void pascal(int rows)
 {
     int coef = 1, space, i, j;
@@ -18,6 +19,41 @@ void pascal(int rows)
             printf("%4d", coef);
         }
 
+        printf("\n");
+    }
+}
+
+////// méthode 2 : en utilisant un tableau
+#define MAX_BUFFER 1024
+
+void pascal2(int n)
+{
+    if (n==1) {
+        printf(" 1 ");
+        return;
+    }
+    
+    int scratch_buffer[MAX_BUFFER] = { 0 };
+    scratch_buffer[0] = 1;
+    int tmp = 0;
+    printf(" 1\n");
+
+    for (int i = 2; i < n+1; i++){
+        tmp = 0;
+
+        for (int j = 0; j < i; j++) {
+            int prev = 0;
+
+            if (j != 0) {
+                prev = scratch_buffer[j - 1];
+                scratch_buffer[j - 1] = tmp;
+            }
+        
+            tmp = prev + scratch_buffer[j];
+            printf(" %d ", tmp);
+        }
+
+        scratch_buffer[i - 1] = tmp;
         printf("\n");
     }
 }
